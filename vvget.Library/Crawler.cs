9bad39dc59e6415c62aa94d6ac8f85a3c40faa49
Amirzhan Alimov <Amirzhan_Alimov@epam.com>
2019-06-17 20:07:07 +0600

    Add project files.

---
+++ vvget.Library/Crawler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vvget.Library
+{
+    public class Crawler
+    {
+        private HttpClient httpClient;
+
+        public Crawler(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public string ToTheSite(Uri url, int level = 0, string extention = "*", bool shouldTrace = false)
+        {
+            using (httpClient)
+            {
+                ReadSite(url);
+            }
+            return null;
+        }
+
+        private void ReadSite(Uri url, int level = 0, string extention = "*", bool shouldTrace = false)
+        {
+            using (HttpResponseMessage response = httpClient.GetAsync(url).Result)
+            {
+                using (HttpContent content = response.Content)
+                {
+                    string subPath = "folder"; // your code goes here
+
+                    bool exists = System.IO.Directory.Exists($""Environment.CurrentDirectory subPath));
+
+                    if (!exists)
+                    {
+                        System.IO.Directory.CreateDirectory(Server.MapPath(subPath));
+                    }
+                    content.ReadAsStringAsync().Result;
+                }
+            }
+        }
+    }
+}
---
+++ vvget/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using vvget.Library;
+
+namespace vvget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Crawler crawler = new Crawler(new HttpClient());
+            var result =  crawler.ToTheSite(new Uri(@"http://kino.kz/"));
+            Console.WriteLine(result);
+            Console.ReadKey(true);
+        }
+    }
+}
